5ff7fa49395b510b7da60d5e975439d888e19f22
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-01-26 20:43:19 +0530

    vikingIdleAnimationStart()

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ var vikingIdleAnimationId = 0;
 
 function vikingIdleAnimation() {
     vikingBackgroundPositionX = vikingBackgroundPositionX - 512.1;
-    viking.style.marginLeft.BackgroundPositionX = vikingBackgroundPositionX
+    viking.style.marginLeft.BackgroundPositionX = vikingBackgroundPositionX + "px";
+}
+
+function vikingIdleAnimationStart() {
+    vikingIdleAnimationId = setInterval(vikingIdleAnimation, 200);
 }
