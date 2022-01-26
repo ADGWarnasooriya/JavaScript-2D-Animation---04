afdcb79b102e100a887762647159cfa06dd3dc7c
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-01-26 20:23:40 +0530

    vikingBackgroundPositionX

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var viking = document.getElementById("viking");
-var vikingIdleAnimationNumber = 0;
+var vikingBackgroundPositionX = 0;
 var vikingIdleAnimationId = 0;
+
+function vikingIdleAnimation() {
+    vikingIdleAnimationNumber = vikingIdleAnimationNumber - 512.1;
+    viking.style.marginLeft = vikingIdleAnimationNumber
+}
