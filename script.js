f006affea7c38d289784229795c1377585acabf6
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-01-26 20:50:54 +0530

    troll js

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ var viking = document.getElementById("viking");
 var vikingBackgroundPositionX = 0;
 var vikingIdleAnimationId = 0;
 
+var troll = document.getElementById("troll");
+var trollBackgroundPossitionX = 0;
+var trollIdleAnimationId = 0;
+
 function vikingIdleAnimation() {
     vikingBackgroundPositionX = vikingBackgroundPositionX - 512.1;
-    viking.style.marginLeft.BackgroundPositionX = vikingBackgroundPositionX + "px";
+    viking.style.BackgroundPositionX = vikingBackgroundPositionX + "px";
 }
 
 function vikingIdleAnimationStart() {
     vikingIdleAnimationId = setInterval(vikingIdleAnimation, 200);
+}
+
+function trollIdleAnimation() {
+    trollBackgroundPossitionX = trollBackgroundPossitionX - 420;
+    troll.style.BackgroundPossitionX = trollBackgroundPossitionX + "px";
+}
+
+function trollIdleAnimationStart() {
+    trollIdleAnimationId = setInterval(trollIdleAnimation, 200);
 }
